83037979fe059608c802da816014b6402ff3e790
Thinh Truong Cong <truongcongthinh.contact@gmail.com>
2023-08-28 23:28:33 +0700

    Refactor HTML CSS depending on code convention

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
   // Đăng kí Click event cho nav-item
-  $(".nav-item").click(function () {
-    $(".nav-item").removeClass("active");
-    $(this).addClass("active");
+  $('.bl_navBar_navItem').click(function () {
+    $('.bl_navBar_navItem').removeClass('active');
+    $(this).addClass('active');
   });
   // Đăng kí Click event cho .categories h2
-  $(".categories h2").click(function () {
-    $(".categories h2").removeClass("active");
-    $(this).addClass("active");
+  $('.bl_ourProducts_category_item').click(function () {
+    $('.bl_ourProducts_category_item').removeClass('active');
+    $(this).addClass('active');
   });
 });
